532a59b58ab0b3ddd8613de61abd5a1ef188458e
CSAR101 <arathore@smu.edu>
2015-12-08 16:58:22 -0600

    While loops

---
+++ Tutorial 4/src/Application.java
@@ -0,0 +1,25 @@
+
+public class Application {
+	public static void main(String[] args) {
+		
+		int value = 0;
+		
+		while(value < 10)
+		{
+			System.out.println("Hello" + value);
+			
+			value = value + 1;
+		}
+		
+		int testValue = 0;
+		
+		while(testValue < 10) 
+		{
+			
+			System.out.println(testValue);
+			
+			testValue += 1;
+		}
+	}
+
+}
