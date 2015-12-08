dd592d4834a7ef676a347be4a297ce76f4f5ac30
CSAR101 <arathore@smu.edu>
2015-12-08 16:43:25 -0600

    Variables

---
+++ Tutorial 2/src/Application.java
@@ -0,0 +1,29 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int myInt = 88;
+		short myShort = 747;
+		long myLong = 9797;
+		
+		double myDouble = 7.3243;
+		float myFloat = 324.3f;
+		
+		char myChar = 'y';
+		boolean myBoolean = false;
+		
+		byte myByte = 127;
+		
+		System.out.println(myInt);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+
+	}
+
+}
